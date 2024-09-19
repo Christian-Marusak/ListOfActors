d3a36c64dc09ecfcdcc50688eced262437799f40
Christian <kiko22223@gmail.com>
2024-09-19 14:15:40 +0200

    Initial Commit

---
+++ ListOfActors/ListOfActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfActorsApp.swift
+//  ListOfActors
+//
+//  Created by Christián on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListOfActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
